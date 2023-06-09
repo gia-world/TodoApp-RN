8bf0699c6f581689411f05b397cfd430fe1bcb0e
Jihye Jun <jihyejun@actbase.io>
2023-06-09 18:06:28 +0900

    버튼 효과(TouchableNativeFeedback,TouchableOpacity)

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -1,8 +1,28 @@
 import React, {useState} from 'react';
-import {StyleSheet, TextInput, View} from 'react-native';
+import {
+  Image,
+  Keyboard,
+  Platform,
+  StyleSheet,
+  TextInput,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 const AddTodo = () => {
   const [text, setText] = useState('');
+
+  const onPress = () => {
+    setText('');
+    Keyboard.dismiss();
+  };
+
+  const button = (
+    <View style={styles.buttonStyle}>
+      <Image source={require('../assets/icons/add_white/add_white.png')} />
+    </View>
+  );
 
   return (
     <View style={styles.block}>
@@ -11,7 +31,42 @@ const AddTodo = () => {
         style={styles.input}
         value={text}
         onChangeText={setText}
+        onSubmitEditing={onPress}
+        returnKeyType="done"
       />
+      {/* {Platform.OS === 'ios' ? (
+        <TouchableOpacity activeOpacity={0.5}>
+          <View style={styles.buttonStyle}>
+            <Image
+              source={require('../assets/icons/add_white/add_white.png')}
+            />
+          </View>
+        </TouchableOpacity>
+      ) : (
+        <View style={styles.circleWrapper}>
+          <TouchableNativeFeedback>
+            <View style={styles.buttonStyle}>
+              <Image
+                source={require('../assets/icons/add_white/add_white.png')}
+              />
+            </View>
+          </TouchableNativeFeedback>
+        </View>
+      )} */}
+      {Platform.select({
+        ios: (
+          <TouchableOpacity activeOpacity={0.5} onPress={onPress}>
+            {button}
+          </TouchableOpacity>
+        ),
+        android: (
+          <View style={styles.circleWrapper}>
+            <TouchableNativeFeedback onPress={onPress}>
+              {button}
+            </TouchableNativeFeedback>
+          </View>
+        ),
+      })}
     </View>
   );
 };
@@ -23,11 +78,25 @@ const styles = StyleSheet.create({
     borderColor: '#bdbdbd',
     borderTopWidth: 1,
     borderBottomWidth: 1,
-    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   input: {
     fontSize: 16,
     paddingVertical: 8,
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 48,
+    height: 48,
+    backgroundColor: '#26a69a',
+    borderRadius: 24,
+  },
+  circleWrapper: {
+    borderRadius: 24,
+    overflow: 'hidden',
   },
 });
 
